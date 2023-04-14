f55e47fcfd5281fbc8d3d58b48338b91f2756502
rhixecompany <admin@rhixe.company>
2023-04-14 23:03:33 +0100

    Updates

--- image_downloader/pipelines/getimages.py
+++ image_downloader/pipelines/getimages.py
@@ -79,8 +79,8 @@ class DownloaderImagesPipeline(ImagesPipeline):
     def item_completed(self, results, item, info):
         image_paths = [x['path'] for ok, x in results if ok]
         if not image_paths:
-            print("Item contains no images")
-            # raise DropItem("Item contains no images")
+
+            raise DropItem("Item contains no images")
         adapter = ItemAdapter(item)
         adapter['image_paths'] = image_paths
         return item
